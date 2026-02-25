3adafcfd48a83193dd99ae03ea756cc351f239dc
万耀中 <wanyaozhong@moyaozhongdeMacBook-Pro-2.local>
2026-02-25 20:47:19 +0800

    feat(prompt_lab_core): define SQL-first v2 schema

---
+++ prompt_lab_core/migrations/202602250001_init_prompt_lab_sqlite_v2.down.sql
@@ -0,0 +1,6 @@
+DROP TABLE IF EXISTS golden_set_items;
+DROP TABLE IF EXISTS ai_execution_logs;
+DROP TABLE IF EXISTS check_results;
+DROP TABLE IF EXISTS checklist_items;
+DROP TABLE IF EXISTS sop_steps;
+DROP TABLE IF EXISTS sops;
